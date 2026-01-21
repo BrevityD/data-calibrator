d12cd6dd0583fdff4cc8f48b1d293ba62c90bd47
Brevity_D <brevity_d@mail.ustc.edu.cn>
2026-01-21 15:21:39 +0800

    feat: exp0: ceiling in code and math domain

---
+++ samples/experiment0/ceiling/run_math_exp.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+
+## =======
+current_file_path = Path(__file__).resolve()
+project_root = current_file_path.parents[3]
+import sys
+sys.path.append(str(project_root))
+## =======
+
+from datacalibrator.datasets.math_adaptor import get_math_dataset
+from datacalibrator.seed import SEED
+
+from trl import SFTTrainer, SFTConfig
+
+os.environ["WANDB_PROJECT"] = "data-calibrator-exp0"
+max_seq_length = 16384
+
+train_dataset, test_dataset = get_math_dataset()
+
+trainer = SFTTrainer(
+    model = "/models/Qwen3-1.7B",
+    train_dataset = train_dataset,
+    eval_dataset = test_dataset,
+    args = SFTConfig(
+        do_eval = True,
+        eval_strategy = "steps",
+        eval_steps = 1,
+        eval_on_start = True,
+        max_length = max_seq_length,
+        learning_rate = 2e-7,
+        per_device_train_batch_size = 4,  # 64 * `epoch` steps
+        num_train_epochs = 15,
+        gradient_accumulation_steps = 4,
+        logging_steps = 1,
+        output_dir = "outputs-1_7B-math-ceiling-5e7-bs16-ep15-signsgd",
+        # optim = "sgd",
+        adam_beta1 = 1e-12,
+        adam_beta2 = 1e-12,
+        lr_scheduler_type = "constant",
+        seed = SEED,
+        save_strategy = "steps",
+        save_steps = 0.02,
+        save_only_model = True,
+        run_name = "math-1_7B-ceiling-5e7-bs16-ep15-signsgd",
+        report_to = "wandb"
+    ),
+)
+trainer.train()
